89280fc8ca7b7fd6d4dd36e6c2bfdb588405441d
Isa Aliev <isaaliev12@gmail.com>
2023-12-14 15:51:29 +0300

    ..

--- Sources/XrayKit/XrayCore.swift
+++ Sources/XrayKit/XrayCore.swift
@@ -5,16 +5,17 @@ import LibXray
 public enum XrayCore {
     private static var running_on_pid: pid_t = 0
 
-    public static func run(config url: URL, assets: URL, _ completion: @escaping (NEVPNError?) -> ()) {
+    public static func run(config url: URL, assets: URL, logger: @escaping (String) -> Void, _ completion: @escaping (NEVPNError?) -> ()) {
         DispatchQueue.global(qos: .userInitiated).async {
             running_on_pid = getpid()
             guard running_on_pid > 0 else {
-                print("XrayCore cannot be running on pid \(running_on_pid). Bailing out.")
+                logger("XrayCore cannot be running on pid \(running_on_pid). Bailing out.")
                 let error = NEVPNError(.configurationInvalid)
                 completion(error)
                 return
             }
                 
+            logger("get pid \(running_on_pid)")
             let args: [String] = ["libxray", "-c", url.path]
             var argv = args.map { strdup($0) }
             let argc = Int32(argv.count)
@@ -23,8 +24,10 @@ public enum XrayCore {
             var envv = env.map { strdup($0) }
             let envc = Int32(envv.count)
 
+            logger("libxray_main call")
             let result = libxray_main(argc, &argv, envc, &envv)
-
+            logger("libxray_main return \(result)")
+            
             for ptr in envv { free(ptr) }
             for ptr in argv { free(ptr) }
 
